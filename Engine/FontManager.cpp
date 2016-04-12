044c3031531cb6cfc6f590ffc0c789734a368e16
OGDecode <linus.skold.sp14@thegameassembly.com>
2016-04-13 01:21:50 +0200

    Small change. Still doesn't work.

--- Engine/FontManager.cpp
+++ Engine/FontManager.cpp
@@ -53,12 +53,20 @@ namespace Snowblind
 		error = FT_Set_Pixel_Sizes(myFace, 0, aFontHeight);
 		DL_ASSERT_EXP(!error, "Failed to set pixel size!");
 
-		for (int i = 0; i < 128; i++) // I want to loop the first 256 characters
+		for (int i = 0; i < 128; i++) 
 		{
-			int glyphIndex = FT_Get_Char_Index(myFace, i);
-			error = FT_Load_Glyph(myFace, i, FT_LOAD_RENDER);
-			DL_ASSERT_EXP(!error, "Failed to load glyph!");
+			//int glyphIndex = FT_Get_Char_Index(myFace, i);
+			/*error = FT_Load_Glyph(myFace, i, FT_LOAD_DEFAULT);
+			if(error)
+				continue;*/
+			error = FT_Load_Char(myFace, i, FT_LOAD_RENDER);
+			if (error)
+				continue;
+			//DL_ASSERT_EXP(!error, "Failed to load glyph!");
+
 			FT_GlyphSlot slot = myFace->glyph;
+
+
 			FT_Bitmap bitmap = slot->bitmap;
 			int width = bitmap.width;
 			int height = bitmap.rows;
@@ -73,15 +81,15 @@ namespace Snowblind
 			{
 				for (int x = 0; x < bitmap.width; x++)
 				{
-					gData[2 * (x + y * w)] = 255;
-					gData[2 * (x + y * w) + 1] = bitmap.buffer[x + width * y];
+					//gData[2 * (x + y * w)] = 255;
+					gData[2 * (x + y * w)] = bitmap.buffer[x + width * y];
 				}
 			}
 
 
 			D3D11_SUBRESOURCE_DATA data;
 			data.pSysMem = gData;
-			data.SysMemPitch = width * aFontHeight;
+			data.SysMemPitch = width * height;
 
 			D3D11_TEXTURE2D_DESC info;
 			info.Width = width;
@@ -91,7 +99,7 @@ namespace Snowblind
 			info.SampleDesc.Count = 1;
 			info.SampleDesc.Quality = 0;
 			info.MiscFlags = 0;
-			info.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+			info.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
 			info.Usage = D3D11_USAGE_DYNAMIC;
 			info.BindFlags = D3D11_BIND_SHADER_RESOURCE;
 			info.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
@@ -100,8 +108,8 @@ namespace Snowblind
 			myDevice->CreateTexture2D(&info, &data, &texture);
 			DL_ASSERT_EXP(texture != nullptr, "Texture is nullptr!");
 			std::stringstream ss;
-			ss << "Glyph_" << i << ".png";
-			HRESULT hr = D3DX11SaveTextureToFile(CEngine::GetInstance()->GetAPI()->GetContext(), texture, D3DX11_IMAGE_FILE_FORMAT::D3DX11_IFF_PNG, ss.str().c_str());
+			ss << "Glyph_" << i << ".dds";
+			HRESULT hr = D3DX11SaveTextureToFile(CEngine::GetInstance()->GetAPI()->GetContext(), texture, D3DX11_IMAGE_FILE_FORMAT::D3DX11_IFF_DDS, ss.str().c_str());
 			CEngine::GetInstance()->GetAPI()->HandleErrors(hr, "Failed to save texture because : ");
 
 			delete[] gData;

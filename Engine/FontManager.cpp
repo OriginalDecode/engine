95e8b341fd646dafd249093c06c8dc7468c6917d
OGDecode <linus.skold.sp14@thegameassembly.com>
2016-04-14 19:44:58 +0200

    Still rendering 4 letters

--- Engine/FontManager.cpp
+++ Engine/FontManager.cpp
@@ -3,6 +3,7 @@
 #include <DL_Debug.h>
 #include <ft2build.h>
 #include FT_FREETYPE_H
+#include FT_GLYPH_H
 
 #include <D3DX11.h>
 #include <D3D11.h>
@@ -43,32 +44,28 @@ namespace Snowblind
 		FONT_LOG("Loading font:%s", myFontPath);
 		DL_ASSERT_EXP(!error, "Failed to load requested font.");
 
-		error = FT_Set_Pixel_Sizes(myFace, aFontWidth, 0);
+		//error = FT_Set_Pixel_Sizes(myFace, aFontWidth, 0);
+		error = FT_Set_Char_Size(myFace, aFontWidth * 64, 0, 300, 300);
 		DL_ASSERT_EXP(!error, "Failed to set pixel size!");
+
+
 
 	//	error = FT_Set_Char_Size(myFace, aFontHeight, aFontHeight, 72, 72);
 
+		CreateDirectory("Glyphs", NULL); //Creates a folder for the glyphs
 
 		for (int i = 65; i < 128; i++)
 		{
-			//int glyphIndex = FT_Get_Char_Index(myFace, i);
-			/*error = FT_Load_Glyph(myFace, i, FT_LOAD_DEFAULT);
-			if(error)
-			continue;*/
 			error = FT_Load_Char(myFace, i, FT_LOAD_RENDER);
 			
-			if (error)
+			if (error) //Replace with better error handling.
 				continue;
 			//DL_ASSERT_EXP(!error, "Failed to load glyph!");
 
-			myFace->glyph->format = FT_GLYPH_FORMAT_BITMAP;
 			FT_GlyphSlot slot = myFace->glyph;
+			slot->format = FT_GLYPH_FORMAT_BITMAP;
 			FT_Bitmap bitmap = slot->bitmap;
-			bitmap.pixel_mode = FT_PIXEL_MODE_GRAY;
-			if (bitmap.buffer == nullptr)
-			{
-				continue;
-			}
+			FT_Render_Glyph(slot, FT_RENDER_MODE_MONO);  
 
 			D3D11_SUBRESOURCE_DATA data;
 			data.pSysMem = bitmap.buffer;
@@ -90,10 +87,13 @@ namespace Snowblind
 			ID3D11Texture2D* texture;
 			myDevice->CreateTexture2D(&info, &data, &texture);
 			DL_ASSERT_EXP(texture != nullptr, "Texture is nullptr!");
+
 			std::stringstream ss;
-			ss << "Glyph_" << i << ".dds";
+			ss << "Glyphs/Glyph_" << i << ".dds";
 			HRESULT hr = D3DX11SaveTextureToFile(CEngine::GetInstance()->GetAPI()->GetContext(), texture, D3DX11_IMAGE_FILE_FORMAT::D3DX11_IFF_DDS, ss.str().c_str());
 			CEngine::GetInstance()->GetAPI()->HandleErrors(hr, "Failed to save texture because : ");
+
+			texture->Release();
 		}
 	}
 

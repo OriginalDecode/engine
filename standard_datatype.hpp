209e97102e7ea8e46cf157fbc6c3bf97436e4ced
OriginalDecode <linus.skold@telia.com>
2016-07-29 04:34:55 +0200

    Started to add typedefs for datatypes.
    Former-commit-id: 1fb43b0d0bb32cc81e7c615c642ca3209b41f6a8

---
+++ standard_datatype.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#if defined(_MSC_VER)
+typedef unsigned __int8 u8;
+typedef __int8 s8;
+#else
+typedef unsigned char u8;
+typedef unsigned char s8;
+#endif
+typedef char c8;
+
+#if defined(_MSC_VER)
+typedef unsigned __int16 u16;
+typedef __int16 s16;
+#else
+typedef unsigned short u16;
+typedef short s16;
+#endif
+
+#if defined(_MSC_VER)
+typedef unsigned __int32 u32;
+typedef __int32 s32;
+#else
+typedef unsigned int u32;
+typedef signed int s32;
+#endif
+
+#if defined(_MSC_VER)
+typedef unsigned __int64 u64;
+typedef __int64 s64;
+#else
+typedef unsigned long long u64;
+typedef long long s64;
+#endif

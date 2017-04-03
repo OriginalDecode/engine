f7bdfdab3329c00cabce80e82c6e537704c31b45
OriginalDecode <linus.skold@telia.com>
2017-04-03 23:37:14 +0200

    Worked on some sky equations.
    Former-commit-id: 58ada563f0bc621c61fa2bfe35f11057d427ff23

--- Workbed/Data/Shaders/PS_Skysphere.hlsl
+++ Workbed/Data/Shaders/PS_Skysphere.hlsl
@@ -27,8 +27,35 @@ struct VS_OUTPUT
 //---------------------------------
 //	Deferred Base Pixel Shader
 //---------------------------------
+
+static const float4 sun_position = float4(2000,700,100,0);
+static const float g = -0.75;
+static const float4 br = float4(5.5e-6, 13.0e-6, 22.4e-6, 0);
+static const float4 bm = float4(21e-6,21e-6,21e-6,0);
+static const float pi = 3.14;
+
+float4 Br(float theta)
+{
+	return 3 / ( 16 * pi ) * br * ( 1 + cos(theta) * cos(theta));
+}
+
+float4 Bm(float theta)
+{
+	return 1 / ( 4 * pi ) * bm * (( 1 - g ) * ( 1 - g )) / ( pow( 1 + g * g - 2 * g * cos(theta), 3 / 2) ); 
+}
+
+float4 atmospheric(float theta, float s)
+{
+	return (Br(theta) * Bm(theta)) / (br + bm) * sun_position * (1 - exp( -(br+bm) * s ));
+}
+
+
 float4 PS(VS_OUTPUT input) : SV_Target
 {
+	
+	float4 lpos = float4(100,0,0,0);
+	float4 cpos = float4(0,0,0,0);
+
 
 
 	float height = input.worldpos.y;
@@ -41,8 +68,13 @@ float4 PS(VS_OUTPUT input) : SV_Target
 	if(depth < 1.f)
 		discard;
 
+	float4 ldir = lpos - input.worldpos;
+	float s = length(input.worldpos - cpos);
+	float theta = acos(dot(normalize(ldir - input.worldpos), normalize(input.worldpos - cpos) * float4(1,0,0,0)));
 
-	
+	float4 output = atmospheric(theta,s) * 100000;
+
+
 	//height = (height - height) * 2.f - 1.f;
 	
 	

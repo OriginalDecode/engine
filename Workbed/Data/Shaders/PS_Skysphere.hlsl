9368d13bacf1f2ccdfb28bd5b2556f8c53701d9e
Linus Sköld <originaldecode@outlook.com>
2017-04-07 13:05:05 +0200

    Download sample from Nvidia to continue.
    Former-commit-id: 10c514fb73e3b94a5ba76d81d612bdfdcb0bb07e

--- Workbed/Data/Shaders/PS_Skysphere.hlsl
+++ Workbed/Data/Shaders/PS_Skysphere.hlsl
@@ -31,47 +31,9 @@ struct VS_OUTPUT
 	float4 tex 		: TEX;
 };
 
-
 //---------------------------------
 //	Deferred Base Pixel Shader
 //---------------------------------
-
-static const float4 ESun = float4(500,250,250,0);
-static const float g = -0.75;
-static const float4 rayleigh_scatter_coef = float4(5.5e-6, 13.0e-6, 22.4e-6, 1);
-static const float mie_scatter_coef = 21e-6;
-static const float pi = 3.14;
-
-
-
-float rayleigh_phase(float theta)
-{
-    return ( 3 / (16 * pi) * (1+  ( 0.5 * (1 - cos(2*theta)) )) );
-}
-
-float4 rayleigh(float theta) //rayleigh
-{
-	return 3 / ( 16 * pi ) * rayleigh_scatter_coef * ( 1 + cos(theta) * cos(theta));
-}
-
-float4 mie(float theta) //mie
-{
-	return 1 / ( 4 * pi ) * mie_scatter_coef * (( 1 - g ) * ( 1 - g )) / ( pow( 1 + g * g - 2 * g * cos(theta), 3 / 2) );
-}
-
-float henyeygreenstein(float theta, float g_in)
-{
-    float neg_gg = ( 1 - g_in ) * ( 1 - g_in );
-    float pos_gg = ( 1 + g_in ) * ( 1 + g_in );
-    float g2 = 2 * g_in;
-
-	return ( neg_gg ) / 4 * pi * pow(( pos_gg ) - ( g2 * cos(theta)), 3/2);
-}
-
-float4 atmospheric(float theta, float s)
-{
-	return (rayleigh(theta) * mie(theta)) / ( rayleigh_scatter_coef + mie_scatter_coef ) * light_position * (1 - exp( -( rayleigh_scatter_coef + mie_scatter_coef ) * s ));
-}
 
 float4 PS(VS_OUTPUT input) : SV_Target
 {
@@ -83,16 +45,6 @@ float4 PS(VS_OUTPUT input) : SV_Target
 
 	if(depth < 1.f)
 		discard;
-	
-	float4 light_dir = normalize( input.worldpos - light_position );
-	float4 view_dir = normalize(input.worldpos - camera_position);
-	float length_of = length(input.worldpos + camera_position);
 
-	float n_theta = acos(dot(normalize(light_dir - input.worldpos), view_dir));
-
- 	//float theta = acos(dot(normalize(light_dir - input.worldpos), normalize(input.worldpos - camera_position) * float4(-1,0,0,0)));
-
-	float4 output = rayleigh(n_theta) * 100000 * 5;// atmospheric(n_theta, length_of) * 1000 * 5;
-	//output = pow( clamp ( output / ( output + 1.0), 0, 1),  1 /2.2);
-	return output;
+	return float4(1,1,1,1);
 }
--- Workbed/Data/Shaders/VS_Skysphere.hlsl
+++ Workbed/Data/Shaders/VS_Skysphere.hlsl
@@ -1,9 +1,9 @@
-//---------------------------------
-//	Deferred Base Vertex Shaders
-//---------------------------------
-//---------------------------------
+//_________________________________
+//	Atmospheric Scattering Vertex Shaders
+//_________________________________
+//_________________________________
 //	Constant Buffers
-//---------------------------------
+//_________________________________
 cbuffer Matrices : register(b0)
 {
 	row_major float4x4 World;
@@ -19,9 +19,9 @@ cbuffer Positions : register(b1)
 	float4 light_pos;
 };
 
-//---------------------------------
-//	Deferred Base Vertex Structs
-//---------------------------------
+//_________________________________
+//	Vertex Structs
+//_________________________________
 struct VS_INPUT
 {
 	float4 pos : POSITION;
@@ -42,11 +42,92 @@ struct VS_OUTPUT
 	float4 tex : TEX;
 };
 
-//---------------------------------
-//	Deferred Base Vertex Shader
-//---------------------------------
+//_________________________________
+//	Atmospheric Scattering Vertex Shader
+//_________________________________
+
+
+// The scale equation calculated by Vernier's Graphical Analysis
+float Scale(float fCos)
+{
+	float x = 1.0 - fCos;
+	return fScaleDepth * exp(-0.00287 + x*(0.459 + x*(3.83 + x*(-6.80 + x*5.25))));
+}
+
+// Calculates the Mie phase function
+float GetMiePhase(float fCos, float fCos2, float g, float g2)
+{
+	return 1.5 * ((1.0 - g2) / (2.0 + g2)) * (1.0 + fCos2) / pow(1.0 + g2 - 2.0*g*fCos, 1.5);
+}
+
+// Calculates the Rayleigh phase function
+float GetRayleighPhase(float fCos2)
+{
+	//return 1.0;
+	return 0.75 + 0.75*fCos2; //is this even correct?
+}
+
+// Returns the near intersection point of a line and a sphere
+float GetNearIntersection(float3 v3Pos, float3 v3Ray, float fDistance2, float fRadius2)
+{
+	float B = 2.0 * dot(v3Pos, v3Ray);
+	float C = fDistance2 - fRadius2;
+	float fDet = max(0.0, B*B - 4.0 * C);
+	return 0.5 * (-B - sqrt(fDet));
+}
+
+float GetFarIntersection(float3 v3Pos, float3 v3Ray, float fDistance2, float fRadius2)
+{
+	float B = 2.0 * dot(v3Pos, v3Ray);
+	float C = fDistance2 - fRadius2;
+	float fDet = max(0.0, B*B - 4.0 * C);
+	return 0.5 * (-B + sqrt(fDet));
+}
+
+static const float3 ESun = float3(20,20,20);
+
 VS_OUTPUT VS(VS_INPUT input)
 {
+	float PI = 3.14159265;
+	int nSamples = 2;
+	float fSamples = (float)nSamples;
+
+	//wavelengths
+	float3 v3InvWavelength = float3(1/pow(0,4),1/pow(0,4),1/pow(0,4));
+
+	//positions
+	float3 v3CameraPos = camera_position;
+	float3 v3LightPos = light_pos;
+	float fCameraHeight = v3CameraPos.y;
+	float fCameraHeight2 = fCameraHeight*fCameraHeight;
+	
+	//Atmosphere radiuses
+	float fOuterRadius = 2.25;
+	float fOuterRadius2 = fOuterRadius*fOuterRadius;
+	float fInnerRadius = 2; 
+	float fInnerRadius2 = fInnerRadius*fInnerRadius;
+
+	//Coefs
+	float3 Kr = float3(0,0,0);
+	float fKrESun = Kr * ESun;
+	float Km = 0;
+	float fKmESun = Km * ESun;
+	float fKr4PI = Kr * 4 * PI;
+	float fKmPI = Km * 4 * PI;
+
+	//Depth stuff
+	float fScaleDepth = 0.25;
+	float fInvScaleDepth = 1 / fScaleDepth;
+	float fScale = 1 / (fOuterRadius - fInnerRadius);
+	float fScaleOverScaleDepth = fScale / fScaleDepth;
+
+
+
+	
+
+
+
+
 	VS_OUTPUT output = (VS_OUTPUT)0;
 
 	output.pos = mul(input.pos, World);
@@ -66,6 +147,6 @@ VS_OUTPUT VS(VS_INPUT input)
 	output.tex = float4((float2(x + w, w - y)) * 0.5f, output.pos.zw);
 	
 	output.worldpos = input.pos;
-	
+
 	return output;
 };

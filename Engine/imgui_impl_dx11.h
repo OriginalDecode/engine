ad736aa2f39cf63bba958ced3d0f5c88ea36f2b2
Linus Sköld <linus.skold@telia.com>
2017-10-22 02:08:52 +0200

    Imgui library updated
    Former-commit-id: 57f0f4b889b1869c5427d8abd856591856812ff2

--- Engine/imgui_impl_dx11.h
+++ Engine/imgui_impl_dx11.h
@@ -5,9 +5,10 @@
 // If you use this binding you'll need to call 4 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXXX_NewFrame(), ImGui::Render() and ImGui_ImplXXXX_Shutdown().
 // If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
 // https://github.com/ocornut/imgui
-#include "imgui.h"
+
 struct ID3D11Device;
 struct ID3D11DeviceContext;
+#include "imgui.h"
 
 IMGUI_API bool        ImGui_ImplDX11_Init(void* hwnd, ID3D11Device* device, ID3D11DeviceContext* device_context);
 IMGUI_API void        ImGui_ImplDX11_Shutdown();

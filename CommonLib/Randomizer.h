e2560a70738122ad7fcbd5700c55e95263d823ff
originaldecode <originaldecode@outlook.com>
2018-11-23 14:54:29 +0100

    Removed randomizer class. no need for it to be a singleton.

--- CommonLib/Randomizer.h
+++ CommonLib/Randomizer.h
@@ -2,41 +2,16 @@
 
 #include <random>
 #include "Utilities.h"
-
-//if you input a higher value in min than max, it will crash. The following must be true. min <= max. Otherwise you will crash.
-#define RANDOM(a,b) Randomizer::GetInstance()->Rand(cl::_min<decltype(a)>(a,b) , cl::_max<decltype(a)>(a,b))
-
-#undef MIN
-#undef MAX
-class Randomizer
+namespace cl
 {
-public:
-
-	static Randomizer* GetInstance();
-	static bool Create();
-	static bool Destroy();
-
 	template<typename T>
-	T Rand(const T& min, const T& max);
-	
-
-
-
-private:
-	Randomizer();
-	~Randomizer();
-
-	static Randomizer* myInstance;
-
-	std::random_device myRandomDevice;
-	std::mt19937 e2;
+	T Rand(const T& min, const T& max)
+	{
+		static std::random_device device;
+		static std::mt19937 e2(device());
+		std::uniform_real_distribution<> dist(min, max);
+		return static_cast<T>(dist(e2));
+	}
 };
 
-template<typename T>
-T Randomizer::Rand(const T& min, const T& max)
-{
-	std::uniform_real_distribution<> dist(min, max);
-
-	return static_cast<T>(dist(e2));
-}
-
+#define RANDOM(a,b) cl::Rand(cl::_min<decltype(a)>(a,b) , cl::_max<decltype(a)>(a,b))
